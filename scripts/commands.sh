1ba72c0dc21a5925a2431c17c5917f62f370e8ac
Luan <Luan.ab2002@yahoo.com.br>
2023-07-27 09:24:31 -0300

    Configuring the Dockerfile to generate our Docker Django imagem

---
+++ scripts/commands.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# The shell will interrupt the script execution when a command fails.
+set -e
+
+while ! nc -z $POSTGRES_HOST $POSTGRES_PORT; do
+  echo "🟡 Waiting for Postgres Database Startup ($POSTGRES_HOST $POSTGRES_PORT) ..." &
+  sleep 0.1
+done
+
+echo "✅ Postgres Database Started Successfully ($POSTGRES_HOST:$POSTGRES_PORT)"
+
+python manage.py collectstatic
+python manage.py migrate
+python manage.py runserver
