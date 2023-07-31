e80395070e2e62c24ead3429133ddaf60de938e6
Luan <Luan.ab2002@yahoo.com.br>
2023-07-31 20:32:30 -0300

    separating scripts to execute better the commands

--- scripts/commands.sh
+++ scripts/commands.sh
@@ -3,13 +3,7 @@
 # The shell will interrupt the script execution when a command fails.
 set -e
 
-while ! nc -z $POSTGRES_HOST $POSTGRES_PORT; do
-  echo "🟡 Waiting for Postgres Database Startup ($POSTGRES_HOST $POSTGRES_PORT) ..." &
-  sleep 0.1
-done
-
-echo "✅ Postgres Database Started Successfully ($POSTGRES_HOST:$POSTGRES_PORT)"
-
-python manage.py collectstatic
-python manage.py migrate
-python manage.py runserver
+wait_pqsl.sh
+collectstatic.sh
+migrate.sh
+runserver.sh
