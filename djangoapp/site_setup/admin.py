996dcc1e6d0833f878e8431ca50200a45f5681d9
Luan <Luan.ab2002@yahoo.com.br>
2023-08-01 20:42:00 -0300

    variable 'inline' in SiteSetupAdmin

--- djangoapp/site_setup/admin.py
+++ djangoapp/site_setup/admin.py
@@ -1,13 +1,12 @@
 from django.contrib import admin
 from site_setup.models import MenuLink, SiteSetup
 
-
-# Register your models here.
-@admin.register(MenuLink)
-class MenuLinkAdmin(admin.ModelAdmin):
-    list_display = ['id', 'text', 'url_or_path']
-    list_display_links = ['id', 'text', 'url_or_path']
-    search_fields = ['id', 'text', 'url_or_path']
+# # Register your models here.
+# @admin.register(MenuLink)
+# class MenuLinkAdmin(admin.ModelAdmin):
+#     list_display = ['id', 'text', 'url_or_path']
+#     list_display_links = ['id', 'text', 'url_or_path']
+#     search_fields = ['id', 'text', 'url_or_path']
 
 
 class MenuLinkInline(admin.TabularInline):
@@ -18,6 +17,7 @@ class MenuLinkInline(admin.TabularInline):
 @admin.register(SiteSetup)
 class SiteSetupAdmin(admin.ModelAdmin):
     list_display = ['title', 'description']
+    inlines = MenuLinkInline,
 
     def has_add_permission(self, request) -> bool:
         return not SiteSetup.objects.exists()
